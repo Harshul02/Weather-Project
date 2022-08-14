6a5784d053b6f3385e1a7ddd46f54e09347be2b0
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-15 00:38:47 +0530

    Fetched API using https.get method

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const https = require("https");
 const app = express();
 
 
 app.get("/", function(req,res){
+
+    const url="https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=844e25a59dcf84f0fb68bd06bf58a3c7";
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data)
+        {
+            console.log(data);
+        })
+    });
     res.send("Server is running");
 })
 
